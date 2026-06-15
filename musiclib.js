829bbddcb3294d4146973cee9b9ea0e704027807
YuEn <45507958+CYE04@users.noreply.github.com>
2026-06-15 19:19:35 +0200

    Add files via upload

--- musiclib.js
+++ musiclib.js
@@ -1,7 +1,7 @@
 /* ✦ Designed & Built by YuEn © 2025–2026 ✦ */
 /* CECP Music Library v3.3 */
 (function(){
-  const ML_VER='2026.06.15.1-layout-smooth';
+  const ML_VER='2026.06.15.2-scroll-hotfix';
   const GITHUB_API='https://api.github.com/repos/CYE04/Cecp/contents/songs';
   const RAW_BASE='https://raw.githubusercontent.com/CYE04/Cecp/main/songs/';
   const HALO_BASE='https://cecp.it';
