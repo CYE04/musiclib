0210ea8c08ca69a22318274f77d7f6b119b60df9
YuEn <45507958+CYE04@users.noreply.github.com>
2026-07-07 19:20:20 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = "cecp-musiclib-pwa-v23-fx-v5";
+const CACHE_NAME = "cecp-musiclib-pwa-v24-fx-v6";
 const DATA_CACHE = "cecp-musiclib-data-v1";
 const APP_SHELL = [
   "./?key=cecp2026",
   "./index.html",
-  "./musiclib.css?v=20260707-fx-v5",
-  "./gsap.min.js?v=20260707-fx-v5",
-  "./pinyin-dict.js?v=20260707-fx-v5",
-  "./musiclib.js?v=20260707-fx-v5",
+  "./musiclib.css?v=20260707-fx-v6",
+  "./gsap.min.js?v=20260707-fx-v6",
+  "./pinyin-dict.js?v=20260707-fx-v6",
+  "./musiclib.js?v=20260707-fx-v6",
   "./manifest.webmanifest",
   "./olive-fellowship-logo.png",
   "./icons/favicon.ico",
